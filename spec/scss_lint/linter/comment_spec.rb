47d1d889c1a8dbf6c3a8dabdda17679c09090324
Matt Diebolt <mdiebolt@gmail.com>
2015-02-23 01:48:07 -0600

    add .sass examples for comment spec

--- spec/scss_lint/linter/comment_spec.rb
+++ spec/scss_lint/linter/comment_spec.rb
@@ -8,11 +8,17 @@ describe SCSSLint::Linter::Comment do
       }
     SCSS
 
+    let(:sass) { <<-SASS }
+      p
+        margin: 0
+    SASS
+
     it { should_not report_lint }
   end
 
   context 'when comment is a single line comment' do
     let(:scss) { '// Single line comment' }
+    let(:sass) { '// Single line comment' }
 
     it { should_not report_lint }
   end
@@ -23,6 +29,11 @@ describe SCSSLint::Linter::Comment do
         margin: 0; // Comment at end of line
       }
     SCSS
+
+    let(:sass) { <<-SASS }
+      p
+        margin: 0 // Comment at end of line
+    SASS
 
     it { should_not report_lint }
   end
@@ -40,6 +51,17 @@ describe SCSSLint::Linter::Comment do
       }
     SCSS
 
+    # TODO: This passes but seems like it shouldn't
+    let(:sass) { <<-SASS }
+      h1
+        color: #eee
+      /*
+       * This is a multi-line comment that should report a lint
+       */
+      p
+        color: #DDD
+    SASS
+
     it { should report_lint line: 4 }
   end
 
@@ -49,6 +71,11 @@ describe SCSSLint::Linter::Comment do
         color: #eee; /* This is a comment */
       }
     SCSS
+
+    let(:sass) { <<-SASS }
+      h1
+        color: #eee /* This is a comment */
+    SASS
 
     it { should report_lint line: 2 }
   end
